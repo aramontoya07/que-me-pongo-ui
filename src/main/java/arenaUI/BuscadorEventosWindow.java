ff3edf9459d023f699a72c45bb3852b76bcd8f69
Great Saiyaman <thesaiyanbot@gmail.com>
2019-08-28 17:23:51 -0300

    Creo las funciones de agregar y quitar eventos. Tambien cambie la vista pero todavia le falta

--- src/main/java/arenaUI/BuscadorEventosWindow.java
+++ src/main/java/arenaUI/BuscadorEventosWindow.java
@@ -10,64 +10,80 @@ import org.uqbar.arena.widgets.tables.Column;
 import org.uqbar.arena.widgets.tables.Table;
 import org.uqbar.arena.windows.MainWindow;
 import org.uqbar.arena.widgets.Button;
+import org.uqbar.lacar.ui.model.PanelBuilder;
+
 
 @SuppressWarnings("serial")
 public class BuscadorEventosWindow extends MainWindow<BuscadorEventos> {
 
-    public BuscadorEventosWindow() {
-        super(new BuscadorEventos());
-//        this.getModelObject().search();
-    }
-    
+
+
+	private BuscadorEventosWindow() {
+		super(new BuscadorEventos());
+	}
+
 	@Override
 	public void createContents(Panel mainPanel) {
 		this.setTitle("Buscador de Eventos");
 		mainPanel.setLayout(new VerticalLayout());
 
-		
+
+
+
 		//TEXTOS PARA FECHA DESDE
 		new Label(mainPanel).setText("Ingrese fecha de inicio (DD/MM/AAAA):");
 
 		new TextBox(mainPanel).bindValueToProperty("fechaDesde").setTransformer(new DateTransformer()); //pasa un string de forma DD/MM/AAAA a Date
-		
+
 		//TEXTOS PARA FECHA HASTA
 		new Label(mainPanel).setText("Ingrese fecha de fin (DD/MM/AAAA):");
 
 		new TextBox(mainPanel).bindValueToProperty("fechaHasta").setTransformer(new DateTransformer());
 
 		//BOTONES BUSCAR Y LIMPIAR
-		new Button(mainPanel)
-			.setCaption("Buscar")
-			.onClick(()-> this.getModelObject().search());
-		
-		new Button(mainPanel)
-			.setCaption("Limpiar")
-			.onClick(()-> this.getModelObject().clear());
+		Panel botonesPanel = new Panel(mainPanel);
+		botonesPanel.setLayout(PanelBuilder::setHorizontalLayout);
+
+		new Button(botonesPanel)
+				.setCaption("Buscar")
+				.onClick(()-> this.getModelObject().search());
+
+		new Button(botonesPanel)
+				.setCaption("Limpiar")
+				.onClick(()-> this.getModelObject().clear());
+
+		new Button(botonesPanel)
+				.setCaption("Agregar Evento")
+				.onClick(()-> System.out.println("gg"));
+
+		new Button(botonesPanel)
+				.setCaption("Eliminar Evento")
+				.onClick(()->System.out.println("gato"));
 
 		//RESULTADOS DE BUSQUEDA
 		Table<BuscadorEventos> tablaEventos = new Table<BuscadorEventos>(mainPanel, BuscadorEventos.class);
 		tablaEventos.bindItemsToProperty("resultados");
-		
-		new Column<BuscadorEventos>(tablaEventos)
-	    	.setTitle("Nombre")
-	    	.setFixedSize(150)
-	    	.bindContentsToProperty("nombre");
 
 		new Column<BuscadorEventos>(tablaEventos)
-			.setTitle("Fecha")
-			.setFixedSize(150)
-			.bindContentsToProperty("fecha");
+				.setTitle("Nombre")
+				.setFixedSize(150)
+				.bindContentsToProperty("nombre");
 
 		new Column<BuscadorEventos>(tablaEventos)
-			.setTitle("Lugar")
-			.setFixedSize(150)
-			.bindContentsToProperty("lugar");
+				.setTitle("Fecha")
+				.setFixedSize(150)
+				.bindContentsToProperty("fecha");
 
 		new Column<BuscadorEventos>(tablaEventos)
-			.setTitle("Tiene Sugerencia")
-			.setFixedSize(150)
-			.bindContentsToProperty("tieneSugerencia");
-		
+				.setTitle("Lugar")
+				.setFixedSize(150)
+				.bindContentsToProperty("lugar");
+
+		new Column<BuscadorEventos>(tablaEventos)
+				.setTitle("Tiene Sugerencia")
+				.setFixedSize(150)
+				.bindContentsToProperty("tieneSugerencia");
+
 	}
 
 	public static void main(String[] args) {
